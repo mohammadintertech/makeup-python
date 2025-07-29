ebfd1660b85d92a9a397d5983ba5eea786ba750c
mohammadintertech <147643125+mohammadintertech@users.noreply.github.com>
2025-07-29 11:16:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -86,15 +86,9 @@ def apply_makeup():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=8081)
+@app.route('/')
+def home():
+    return "Hello from Flask on Render!"
 
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=8081)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run()
